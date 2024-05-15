f575b9c2f76e1fc8dff865962865675fe8874d63
sri <srilakshmiT1207@gmail.com>
2024-05-15 09:26:43 +0530

    practicing

--- mysql.sh
+++ mysql.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 SU=($sudo su -)
-if [ $SU -ne 0 ] ;
+if [ $SU -ne 0 ] 
+then
 echo " you are not in user "
 else
 echo " you are in root user "
 fi
 ID=$(id -u)
-if [ $ID -ne 0 ] ;
+if [ $ID -ne 0 ]
+then
 echo " please take root access "
 else
 echo " you are in root user "
 fi
 
 yum install mysql -y
-if [ $? -ne 0 ] ;
+if [ $? -ne 0 ]
+then
 echo " mysql not installed"
 else
 echo " mysql installation suceess "
