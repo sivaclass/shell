6bb6dd3615a437b29e9d85884b2dd1cf27f3a965
sri <srilakshmiT1207@gmail.com>
2024-05-15 10:21:25 +0530

    practicing

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ID=$(id -u)
 LOGFILE=/tmp/$(date)-$0
-R=/e[31m
-G=/e[32m
-N=/[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 if [ $ID -ne 0 ]
 then
 echo " please take root access "
@@ -13,9 +13,9 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ]
 then
-echo " $2 $R not installed $N"
+echo -e " $2 $R not installed $N"
 else
-echo " $2 installation suceess $G you can proceed $N"
+echo -e " $2 installation suceess $G you can proceed $N"
 fi
    
 }
